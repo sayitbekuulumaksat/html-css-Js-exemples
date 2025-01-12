3bb83877eae7dd00359d1c53d33d6b1d1b721d21
Saiitbekuulu Maksatbek <sayitbekuulumaksat@gmail.com>
2025-01-13 03:46:08 +0600

    added 02 folder Todo form

---
+++ 02-Todo form/script.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function () {
+  const todoForm = document.querySelector(".form");
+  const todoInput = document.querySelector("#input-todo");
+  const todoList = document.querySelector(".todo-list");
+  const todoItim = document.querySelector(".todo-item");
+  todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (todoInput.value !== "") {
+      const todoItim = document.createElement("div");
+      todoItim.innerText = todoInput.value;
+      todoItim.classList.add("todo-item");
+      todoList.appendChild(todoItim);
+      todoInput.value = "";
+    }
+    todoItim.addEventListener("click", function () {
+      todoItim.style.display = "none";
+    });
+  });
+});
